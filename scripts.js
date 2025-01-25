982fc907f772f2bab519eb9e435568ccfcb8da94
sepponevalainen <S.Nevalainen@gmail.com>
2025-01-25 23:04:54 +0200

    Made  section page size, stylized resources, added OpenStreetMap.

--- scripts.js
+++ scripts.js
@@ -11,4 +11,21 @@ function initMap() {
   });
 }
 
-//<script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"></script>
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("DOM fully loaded, initializing map.");
+  // Initialize the map
+  var map = L.map("map").setView([63.0577, 30.3464], 10); // Coordinates for Patvinsuo National Park
+
+  // Add OpenStreetMap tiles
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  // Add a marker for Patvinsuo National Park
+  var marker = L.marker([63.11586232190913, 30.69928421435293]).addTo(map);
+
+  // Add a popup to the marker
+  marker.bindPopup('<b>Patvinsuo National Park</b><br>Experience the beauty of Finnish wetlands.').openPopup();
+});
+
